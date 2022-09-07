8905a146b50e71a958da4990f208d74d96d35284
Xiongtao Dai <xiongtao.dai@hotmail.com>
2022-09-07 05:37:51 -0700

    Make

---
+++ demos/sl3.R
@@ -0,0 +1,43 @@
+set.seed(49753)
+library(tidyverse)
+library(data.table)
+library(SuperLearner)
+library(origami)
+library(sl3)
+
+# load example data set
+data(cpp)
+cpp <- cpp %>%
+  dplyr::filter(!is.na(haz)) %>%
+  mutate_all(~ replace(., is.na(.), 0))
+
+# use covariates of intest and the outcome to build a task object
+covars <- c("apgar1", "apgar5", "parity", "gagebrth", "mage", "meducyrs",
+            "sexn")
+task <- sl3_Task$new(
+  data = cpp,
+  covariates = covars,
+  outcome = "haz"
+)
+
+# set up screeners and learners via built-in functions and pipelines
+slscreener <- Lrnr_pkg_SuperLearner_screener$new("screen.glmnet")
+glm_learner <- Lrnr_glm$new()
+screen_and_glm <- Pipeline$new(slscreener, glm_learner)
+SL.glmnet_learner <- Lrnr_pkg_SuperLearner$new(SL_wrapper = "SL.glmnet")
+
+# stack learners into a model (including screeners and pipelines)
+learner_stack <- Stack$new(SL.glmnet_learner, glm_learner, screen_and_glm)
+stack_fit <- learner_stack$train(task)
+preds <- stack_fit$predict()
+head(preds)
+
+bglm_learner <- Lrnr_bayesglm$new()
+screen_and_bglm <- Pipeline$new(slscreener, bglm_learner)
+SL.bglmnet_learner <- Lrnr_pkg_SuperLearner$new(SL_wrapper = "SL.glmnet")
+
+# stack learners into a model (including screeners and pipelines)
+learner_stack <- Stack$new(SL.bglmnet_learner, bglm_learner, screen_and_bglm)
+stack_fit <- learner_stack$train(task)
+preds <- stack_fit$predict()
+head(preds)
