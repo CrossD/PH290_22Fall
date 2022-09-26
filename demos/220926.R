5e904617ab10a58c82966109a8b88bb5d65c79a8
Xiongtao Dai <xiongtao.dai@hotmail.com>
2022-09-26 09:43:54 -0700

    Make

--- demos/220926.R
+++ demos/220926.R
@@ -44,3 +44,93 @@ dl2
 
 dl2 + dl2
 
+setGeneric("length")
+setMethod("length", "Direction", function(x) length(x@theta))
+setMethod("length", "Location", function(x) length(x@loc))
+length(l2)
+length(dl2)
+showClass("DirectionField")
+
+# getter
+setGeneric("degree", function(object) standardGeneric("degree"))
+setMethod("degree", "Direction", 
+          function(object) object@theta * 360)
+
+# setter
+setGeneric("degree<-", 
+           function(object, value) standardGeneric("degree<-"))
+setMethod("degree<-", "Direction", function(object, value) {
+  object@theta <- value / 360
+  validObject(object)
+  object
+})
+
+degree(d1)
+
+d2 <- d1
+degree(d2) <- degree(d2) / 3
+d2
+
+
+
+library(R6)
+Logger <- R6Class("Logger", 
+  public = list(
+    filepath = character(),
+    log = function(text) {
+      writeLines(text, private$con)
+      private$nlines <- private$nlines + length(text)
+      invisible(self)
+    },
+    initialize = function(name="log.txt") {
+      self$filepath <- name
+      private$con <- file(name, open="w")
+    },
+    finalize = function() {
+      close(private$con)
+      message(sprintf("Logged %d lines", private$nlines))
+    }
+  ),
+  private = list(
+    nlines = 0,
+    con = NULL
+  )
+)
+
+log1 <- Logger$new()
+log1$log(c("data cleaning...", "training..."))
+log1
+
+log2 <- log1
+log2$log("fitting model")
+log2
+log1
+
+f <- function(logger) {
+  logger$log("your model is phenomenal")
+}
+f(log1)
+log1  
+
+rm(log1)
+rm(log2)
+gc()
+
+
+## packages
+setwd("~/Downloads/PH290")
+usethis::create_package("myPackage")
+
+
+devtools::load_all("myPackage")
+
+mod1 <- lm(lifeExp ~ year, gapminder::gapminder)
+mod2 <- lm(lifeExp ~ year + gdpPercap, gapminder::gapminder)
+CompModels(mod1, mod2)
+
+
+
+
+
+
+
